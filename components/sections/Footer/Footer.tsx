f994c9a35050470cbf0a0807b9a57548adff6577
Vinolia Moekwa <nmoekwa@byupathway.edu>
2026-09-25 16:59:48 +0200

    this is it

--- components/sections/Footer/Footer.tsx
+++ components/sections/Footer/Footer.tsx
@@ -100,21 +100,34 @@ export default function Footer() {
                                             height: "32px",
                                         }}
                                     >
-                                        <img
-                                            src={social.icon}
-                                            alt=""
-                                            width="18"
-                                            height="18"
-                                            style={{
-                                                display: "block",
-                                            }}
-                                        />
+                                        {social.name === "LinkedIn" ? (
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 24 24"
+                                                width="18"
+                                                height="18"
+                                                fill="currentColor"
+                                                aria-hidden="true"
+                                            >
+                                                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.026-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V8.999h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.287zM5.337 7.433a2.062 2.062 0 1 1 0-4.123 2.062 2.062 0 0 1 0 4.123zM3.555 20.452h3.564V8.999H3.555v11.453z" />
+                                            </svg>
+                                        ) : (
+                                            <img
+                                                src={social.icon}
+                                                alt=""
+                                                width="18"
+                                                height="18"
+                                                style={{
+                                                    display: "block",
+                                                }}
+                                            />
+                                        )}
                                     </Link>
                                 ))}
                             </div>
 
-                            <Link href="mailto:info@oatle-technologies.co.za">
-                                info@oatle-technologies.co.za
+                            <Link href="mailto:oatle.technologies@gmail.com">
+                                oatle.technologies@gmail.com
                             </Link>
                         </div>
                     </div>
